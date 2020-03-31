c6a79f02a2b180211d7fb1bba0007a34da39297a
Roman Odynets <r.chernenko28@gmail.com>
2020-03-31 19:18:02 +0300

    Create v.0.0.1

---
+++ Searcher/Searcher/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Searcher
+{
+    class Program
+    {
+        public static string file_name = null;
+        public static string template = null;
+        public static string mode;
+        public static string start_disk;
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                Console.ReadLine();
+                return;
+            }
+
+            if(args[1] == "-m")
+            {
+                if(args[2] == "file")
+                {
+                    if (string.IsNullOrWhiteSpace(args[4]))
+                    {
+                        Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                        Console.ReadLine();
+                        return;
+                    }
+                    else
+                    {
+                        file_name = args[0];
+                        mode = "file";
+                    }
+                }
+                else if(args[2] == "template")
+                {
+                    if (string.IsNullOrWhiteSpace(args[4]))
+                    {
+                        Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                        Console.ReadLine();
+                        return;
+                    }
+                    else
+                    {
+                        template = args[0];
+                        mode = "template";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                    Console.ReadLine();
+                    return;
+                }
+                if(args[3] == "-d")
+                {
+                    if(string.IsNullOrWhiteSpace(args[4]))
+                    {
+                        Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                        Console.ReadLine();
+                        return;
+                    } else
+                    {
+                        start_disk = args[4];
+                    }
+                }
+            }
+
+            if(string.IsNullOrWhiteSpace(start_disk) || string.IsNullOrWhiteSpace(mode))
+            {
+                Console.WriteLine("Use: searcher.exe [VALUE] -m [FILE/TEMPLATE] -d [START POSITION]");
+                Console.ReadLine();
+                return;
+            }
+
+            Searcher s = new Searcher(file_name, template, mode, start_disk);
+            s.search_engine();
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Searcher/Searcher/searcher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Searcher
+{
+    class Searcher
+    {
+        private string file_name { get; set; }
+        private string template { get; set; }
+        private string mode { get; set; }
+        public List<string> searched = new List<string>();
+        private string starter_path { get; set; }
+
+        public Searcher(string file_name, string template, string mode, string starter_path)
+        {
+            this.file_name = file_name;
+            this.template = template;
+            this.mode = mode;
+            this.starter_path = starter_path;
+        }
+
+        private void search_in_dir(object path)
+        {
+            try
+            {
+                if (!Directory.Exists((string)path)) return;
+
+                string[] files = new string[1024];
+
+                if (mode == "file")
+                {
+                    files = Directory.GetFiles((string)path);
+                }
+                else if(mode == "template")
+                {
+                    files = Directory.GetFiles((string)path, template);
+                }
+                
+                foreach (var item in files)
+                {
+                    if (item.Contains(file_name))
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                return;
+            }
+            catch (Exception e) { }
+            
+        }
+
+        private void search_in(object path)
+        {
+            try
+            {
+                string[] s = Directory.GetDirectories((string)path);
+                foreach (var i in s)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(search_in), i);
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(search_in_dir), i);
+                }
+            }
+            catch (Exception e) { }
+        }
+
+        private void search(object p)
+        {
+            try
+            {
+                string[] s = Directory.GetDirectories(starter_path);
+                search_in_dir(starter_path);
+
+
+                foreach (var i in s)
+                {
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(search_in), i);
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(search_in_dir), i);
+                }
+            }
+            catch (Exception e) { }
+        }
+
+        public void search_engine()
+        {
+            int MaxThreadsCount = Environment.ProcessorCount * 16;
+            // Установим максимальное количество рабочих потоков
+            ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+            // Установим минимальное количество рабочих потоков
+            ThreadPool.SetMinThreads(2, 2);
+            ThreadPool.QueueUserWorkItem(new WaitCallback(search));
+        }
+    }
+}
